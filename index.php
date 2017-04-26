94d7a46a9429bdb68aa71463a90fd5a0beee6fb7
Er1v4ldo <eri.barbosarj@gmail.com>
2017-04-26 01:34:47 -0300

    Add cabeçalho e rodapé

--- index.php
+++ index.php
@@ -1,27 +1,45 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <meta charset="UTF-8">
-        <!--Destivando o zoom para telas de Celular-->
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-        <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
-        <link rel="stylesheet" href="estilo.css">
+        <meta charset="UTF-8" />
+        <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css" />
+        <link rel="stylesheet" href="estilo_index.css" />
+        <meta name="viewport" content="width=device-width" />
+        <style>
+            .navbar-header button {
+                color: #fff
+            }
+        </style>
 
         <title>e-Delivery Acesso</title>
 
     </head>
     <body>
+        <header>
+            <nav class="navbar navbar-inverse">
+                <div class="navbar-header">
+                    <a href="#" class="navbar-brand">e-Delivery</a>
+                    <button type="button" data-target=".navbar-collapse" data-toggle="collapse" class="navbar-toggle">Menu</button>
+                </div>
+                <div class="collapse navbar-collapse">
+                    <ul class="navbar-nav nav navbar-right">
+                        <li><a href="#">Cadastre sua Loja</a></li>
+                        <li><a href="#">Cadastre-se</a></li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
 
-        <div id="form-container" class="container-fluid">
+        <div class="container-fluid">
 
-        <div class="container-fluid" id="form-box">
-            <form action="verificar-login.php" method="post">
+            <div id="form-container" class="container-fluid">
+                <form action="verificar-login.php" method="post">
                     <!--Uso do panel-->
                     <div class="panel-group">
                         <div class="panel panel-primary">
                             <div class="panel-heading">Acesso ao sistema</div>
                             <div class="panel-body">
-                                <!-- Campo usuario -->
+                                <!--Campo usuario--> 
                                 <div class="form-group">
                                     <label class="sr-only"for="login" >Usuário</label>
                                     <div class="input-group">
@@ -41,12 +59,12 @@
                                         <input type="password" name="senha" class="form-control" placeholder="sua senha">
                                     </div>
                                 </div>
-                                    <?php
-                                    //se existe
-                                    if (isset($_GET["msg"])) {
-                                        echo $_GET["msg"];
-                                    }
-                                    ?>
+                                <?php
+                                //se existe
+                                if (isset($_GET["msg"])) {
+                                    echo $_GET["msg"];
+                                }
+                                ?>
                                 <div>
                                     <a href="#">Esqueceu sua senha</a>.
                                     <a href="#" class="text-center">Realize seu Cadastro</a>.
@@ -62,5 +80,18 @@
             </div>
         </div>
 
+        <footer>
+            <address>
+                <strong>e-Delivery</strong><br>
+                Av. Martin Luther King Jr., 1864, Del Castilho <br>
+                Rio de Janeiro, RJ <br>
+                Tel.: (21) 3548-4598
+            </address>
+            <address>
+                Email: edelivery@email.com
+            </address>
+        </footer>
+        <script src="bootstrap/js/jquery-2.2.0.min.js" type="text/javascript"></script>
+        <script src="bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
     </body>
 </html>
